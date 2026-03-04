2549084c0c20b483b35108f6ce12d338e36928b4
justerik <sebo.erik@gmail.com>
2026-03-04 14:15:34 +0100

    added a config file, fixed a few issues with silverfish spawning and entity damage registry

--- src/main/java/net/withrage/infestationbackport/PotionOfInfestationBackport.java
+++ src/main/java/net/withrage/infestationbackport/PotionOfInfestationBackport.java
@@ -2,6 +2,7 @@ package net.withrage.infestationbackport;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.withrage.infestationbackport.config.InfestationConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,7 @@ public class PotionOfInfestationBackport implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		InfestationConfig.load();
 		ModEffects.register();
 		ModPotions.register();
 		ModCreative.init();
